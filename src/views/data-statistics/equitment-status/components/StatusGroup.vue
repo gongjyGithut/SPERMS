d708fb762637fd468fe61e0a379c0b16064a7bb5
gongjyGithut <gongjyemail@163.com>
2019-10-22 17:27:06 +0800

    样式调整

--- src/views/data-statistics/equitment-status/components/StatusGroup.vue
+++ src/views/data-statistics/equitment-status/components/StatusGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="status-group">
     <div class="status-group-wrapper">
-      <div class="group-icon">
+      <div class="group-icon icon-inline">
         <svg-icon icon-class="inline" class-name="card-panel-icon" />
         <span>在线</span>
       </div>
@@ -10,8 +10,8 @@
         22
       </div>
     </div>
-    <div class="status-group-wrapper">
-      <div class="group-icon">
+    <div class="status-group-wrapper ">
+      <div class="group-icon icon-outline">
         <svg-icon icon-class="outline" class-name="card-panel-icon" />
         <span>离线</span>
       </div>
@@ -21,25 +21,25 @@
       </div>
     </div>
     <div class="status-group-wrapper">
-      <div class="group-icon">
+      <div class="group-icon icon-breakdown">
         <svg-icon icon-class="breakdown" class-name="card-panel-icon" />
         <span>故障</span>
 
       </div>
 
       <div class="group-text">
-        22台
+        22
       </div>
     </div>
     <div class="status-group-wrapper">
-      <div class="group-icon">
+      <div class="group-icon icon-closedown">
         <svg-icon icon-class="closedown" class-name="card-panel-icon" />
         <span>停机</span>
 
       </div>
 
       <div class="group-text">
-        22台
+        22
       </div>
     </div>
   </div>
@@ -48,6 +48,9 @@
 <script>
 export default {
   name: 'StatusGroup',
+  props:{
+    
+  },
   data() {
     return {
 
@@ -59,20 +62,39 @@ export default {
 div {display: inline-block;}
 .status-group{
   width: 100%;
-  font-size:0px;
+  font-size:0px;//解决inline-block增加的间距
   .status-group-wrapper{
-    font-size: 24px;
-    width: calc(100% / 4);
+    padding-top: 30px;
     height: 100px;
-    line-height: 100px;
+    // line-height: 20px;
+    width: calc(100% / 4);
     text-align: center;
     cursor: pointer;
     border-bottom: 1px solid #c2c2c2;
-    // border:1px solid #c2c2c2;
     .group-icon{
-      margin-right: 15px;
+      width: 100%;
+      font-size: 24px;
+      border-radius:50%;
+      .card-panel-icon{
+        margin-right: 20px;
+      }
+    }
+    .icon-inline{
+      color:#007AEB;
+    }
+    .icon-outline{
+      color:#E6A23C;
+    }
+    .icon-breakdown{
+      color:#EE5D39;
+    }
+    .icon-closedown{
+      color:#909399;
     }
     .group-text{
+      color: #5b6268;
+      font-size: 20px;
+      margin-top: 10px;
     }
     &:not(:last-child) {
       border-right: 1px solid #c2c2c2;
--- src/views/data-statistics/sales-account/index.vue
+++ src/views/data-statistics/sales-account/index.vue
@@ -1,12 +1,61 @@
 <template>
   <div class="app-container">
-    <status-group/>
+
+    <el-row class="search-form">
+      <el-form :inline="true">
+        <el-form-item label="">
+          <el-date-picker v-model="startTime" placeholder="开始时间"/>
+        </el-form-item>
+
+        <el-form-item label="">
+          <el-date-picker v-model="endTime" placeholder="结束时间"/>
+        </el-form-item>
+
+        <el-form-item label="">
+          <el-input placeholder=" 客户编号或名称"/>
+        </el-form-item>
+
+        <el-form-item label="">
+          <el-button type="primary" icon="el-icon-search" @click="handleSearch"/>
+        </el-form-item>
+      </el-form>
+
+    </el-row>
+
+    <el-row :gutter="10" class="table-container">
+      <el-col :span="6">
+        <el-table :data="tableData" border >
+          <el-table-column label="客户名称" prop="customerName"/>
+          <el-table-column label="设备数量"/>
+          <el-table-column label="销售总额" />
+
+          <empty-container slot="empty" class="empty"/>
+        </el-table>
+
+      </el-col>
+
+      <el-col :span="12"/>
+    </el-row>
+
   </div>
 </template>
 <script>
-import StatusGroup from './components/StatusGroup'
+import EmptyContainer from '@/components/EmptyContainer'
 export default {
   name: '',
-  components: { StatusGroup }
+  components: { EmptyContainer }
 }
 </script>
+<style lang="scss" scoped>
+.app-container{
+  background-color: #f5f4f4;
+}
+.table-container{
+  background-color: #fff;
+  height: 500px;
+
+  .empty{
+    text-align: center;
+  }
+}
+</style>
